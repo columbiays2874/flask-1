274b5d74248811f8e832b9884195baabd95df43b
Yuchen Shi <yuchenshi12@gmail.com>
2016-03-27 11:08:23 -0400

    Update flaskr.py

--- examples/flaskr/flaskr.py
+++ examples/flaskr/flaskr.py
@@ -34,4 +34,12 @@ def connect_db():
 if __name__ == '__main__':
     app.run()
 
+from contextlib import closing
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
 
