da746252b9eea0e551f669f35a4e6de62a0cb5e7
Yuchen Shi <yuchenshi12@gmail.com>
2016-03-27 10:55:10 -0400

    Update flaskr.py

--- examples/flaskr/flaskr.py
+++ examples/flaskr/flaskr.py
@@ -44,21 +44,9 @@ def init_db():
         db.cursor().executescript(f.read())
     db.commit()
 
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+@app.before_request
+def before_request():
+    g.db = connect_db()
 
 
 @app.teardown_appcontext
